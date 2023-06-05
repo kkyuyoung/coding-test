906244e6a658f7fc3c88280a62c031b3d04a9eb1
gksrb <gksrb@192.168.0.64>
2023-06-05 10:53:43 +0900

    프로그래머스 코딩테스트 연습 > 완전탐색 > 카펫

---
+++ src/bruteforce/Practice1.java
@@ -0,0 +1,40 @@
+package bruteforce;
+
+/**
+ * 프로그래머스 코딩테스트 연습 > 완전탐색 > 카펫
+ * 
+ * 
+ * @author gksrb
+ *
+ */
+public class Practice1 {
+
+	public static void main(String[] args) {
+	}
+
+	public static int[] solution(int brown, int yellow) {
+		int[] answer = new int[2];
+
+		int total = brown + yellow;
+
+		for (int i = 1; i <= total; i++) {
+			if (total % i == 0) {
+				int width = i;
+				int height = total / width;
+
+				if (width < height) {
+					continue;
+				}
+
+				if (yellow == (width - 2) * (height - 2)) {
+					answer[0] = width;
+					answer[1] = height;
+					return answer;
+				}
+			}
+		}
+
+		return answer;
+	}
+
+}
