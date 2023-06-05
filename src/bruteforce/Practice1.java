18acee5cb613d9b1e42213f7dfb0f1808587c8d2
gksrb <gksrbdud2@naver.com>
2023-06-05 10:56:59 +0900

    -

--- src/bruteforce/Practice1.java
+++ src/bruteforce/Practice1.java
@@ -10,7 +10,7 @@ package bruteforce;
 public class Practice1 {
 
 	public static void main(String[] args) {
-	}
+	} 
 
 	public static int[] solution(int brown, int yellow) {
 		int[] answer = new int[2];
