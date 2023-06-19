7864639158c080395c5944279d37838154cde632
gksrb <gksrbdud2@naver.com>
2023-06-19 22:26:34 +0900

    -

--- src/etc/Practice54.java
+++ src/etc/Practice54.java
@@ -1,5 +1,8 @@
 package etc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -13,19 +16,29 @@ public class Practice54 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
 		while (sc.hasNext()) {
 			int n = sc.nextInt();
+			
 			int answer = 1;
 			int[] arr = new int[n];
+			
+			List<Integer> list = new ArrayList<>();
 			for (int i = 0; i < n; i++) {
 				arr[i] = sc.nextInt();
 				if (i != 0 && arr[i - 1] < arr[i]) {
 					answer++;
 				}
+				if (i != 0 && arr[i - 1] >= arr[i]) {
+					list.add(answer);
+					answer = 1;
+				}
 			}
-			System.out.println(answer);
+			
+			Integer max = Collections.max(list);
+			System.out.println(max);
 		}
+		sc.close();
 	}
 
 }
