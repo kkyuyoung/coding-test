da1a0f1ee78f104ff31fc0f2fe77a6952f7136b0
gksrb <gksrbdud2@naver.com>
2023-07-02 15:52:06 +0900

    백준 > 나이순 정렬 수정

--- src/etc/Practice56.java
+++ src/etc/Practice56.java
@@ -1,8 +1,7 @@
 package etc;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,41 +11,29 @@ import java.util.Scanner;
  *
  */
 public class Practice56 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+	public static void main(String[] args) {
 
-        List<Member> members = new ArrayList<>();
+		Scanner in = new Scanner(System.in);
 
-        for (int i = 0; i < N; i++) {
-            int age = scanner.nextInt();
-            String name = scanner.next();
-            members.add(new Member(age, name));
-        }
+		int N = in.nextInt();
+		String[][] arr = new String[N][2];
 
-        members.sort(new Comparator<Member>() {
-            @Override
-            public int compare(Member m1, Member m2) {
-                if (m1.age == m2.age) {
-                    return members.indexOf(m1) - members.indexOf(m2);
-                }
-                return m1.age - m2.age;
-            }
-        });
+		for (int i = 0; i < N; i++) {
+			arr[i][0] = in.next();
+			arr[i][1] = in.next();
+		}
 
-        for (Member member : members) {
-            System.out.println(member.age + " " + member.name);
-        }
-    }
-}
+		Arrays.sort(arr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
 
+		});
 
-class Member {
-    int age;
-    String name;
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
 
-    Member(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
+	}
 }
