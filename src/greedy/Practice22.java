fe9bb2fb23673e1ad673a5b6722674178e550624
gksrb <gksrbdud2@naver.com>
2023-10-13 22:44:10 +0900

    백준 > 탐욕법 > 블로그2(20365번)

---
+++ src/greedy/Practice22.java
@@ -0,0 +1,32 @@
+package greedy;
+
+import java.util.Scanner;
+
+/**
+ * 백준 > 탐욕법 > 블로그2(20365번)
+ * 
+ * @author gksrbdud
+ *
+ */
+public class Practice22 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int bCnt = 0;
+		int rCnt = 0;
+		
+		String str = sc.next();
+	
+		for(int i=0; i<n; i++) {
+			if(str.charAt(i) == 'B') {
+				bCnt++;
+			}
+			if(str.charAt(i) == 'R') {
+				rCnt++;
+			}
+		
+		}
+
+		System.out.println(Math.min(rCnt, bCnt) + 1);
+	}
+}
