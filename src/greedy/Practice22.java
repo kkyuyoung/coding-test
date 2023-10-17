0f969ce7a57521a7b6081fb3de3974ddc491ea93
gksrb <gksrbdud2@naver.com>
2023-10-17 23:49:50 +0900

    -

--- src/greedy/Practice22.java
+++ src/greedy/Practice22.java
@@ -1,6 +1,7 @@
 package greedy;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * 백준 > 탐욕법 > 블로그2(20365번)
@@ -10,28 +11,22 @@ import java.util.Scanner;
  */
 public class Practice22 {
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String problem = br.readLine();
 		int bCnt = 0;
 		int rCnt = 0;
-		
-		String str = sc.next();
-	
-		for(int i=0; i<n; i++) {
-			if(str.charAt(i) == 'B') {
-				bCnt++;
+		char bf = '\0';
+		for (int i = 0; i < n; i++) {
+			char cur = problem.charAt(i);
+			if (cur != bf) {
+				if (cur == 'R')
+					rCnt++;
+				else
+					bCnt++;
 			}
-			if(str.charAt(i) == 'R') {
-				rCnt++;
-			}
-		
+			bf = cur;
 		}
-
-		if(rCnt >= bCnt) {
-			System.out.println(bCnt + 1);
-		} else {
-			System.out.println(rCnt + 1);
-		}
-		
+		System.out.println(Math.min(rCnt, bCnt) + 1);
 	}
 }
