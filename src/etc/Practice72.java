2eafcdc10858475f088dacb2d3854f830076f412
gksrbdud <gksrbdud2@naver.com>
2024-02-15 10:16:26 +0900

    -

---
+++ src/etc/Practice72.java
@@ -0,0 +1,17 @@
+package etc;
+
+import java.io.IOException;
+import java.util.Stack;
+
+/**
+ * 
+ * 
+ * @author gksrbdud
+ *
+ */
+public class Practice72 {
+	public static void main(String[] args) throws IOException {
+
+	}
+	
+}
